dc24bb07ed069d55506bcee3fd47a0426195fb5f
Nishant Mandhare <nishantmandhare2004@gmail.com>
2026-09-22 11:34:41 +0530

    Add basic backend folder structure with routes and controllers

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const customerRoutes = require("./src/routes/customerRoutes");
 
 const app = express();
 
 const PORT = 5000;
+
+app.use("/api/customers", customerRoutes);
 
 app.get("/", (req, res) => {
     res.send("Backend server is running!");
---
+++ backend/src/controllers/customerController.js
@@ -0,0 +1,5 @@
+const getAllCustomers = (req, res) => {
+  res.send("Here we will send all customers list");
+};
+
+module.exports = { getAllCustomers };
